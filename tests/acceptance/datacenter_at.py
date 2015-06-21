eeb2ac77fd7833338d90138b4e2641ddc19ee833
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-06-21 19:49:16 +0200

    [ACC-162] init datacenter

---
+++ tests/acceptance/datacenter_at.py
@@ -0,0 +1,35 @@
+# Ariane CLI Python 3
+# Datacenter acceptence tests
+#
+# Copyright (C) 2015 echinopsii
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import json
+import unittest
+from ariane_clip3.directory import DirectoryService
+
+__author__ = 'mffrench'
+
+
+class DriverConfTest(unittest.TestCase):
+
+    def test_datacenter_get(self):
+        args = {'type': 'REST', 'base_url': 'http://localhost:6969/ariane/', 'user': 'yoda', 'password': 'secret'}
+        service = DirectoryService(args)
+        ret = service.datacenter_service.get_datacenters()
+        for datacenter in ret:
+            print(datacenter)
+
+    def test_datacenter_find(self):
+        pass
