a1351e5f01c871aee292ce4cf879b7d2f6a0a6f6
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-07-15 19:34:23 +0200

    [ACC-182] update some PyPi setup

--- setup.py
+++ setup.py
@@ -13,11 +13,14 @@ requirements.append('pykka')
 
 long_description = ('Ariane CLIP3 (client python 3) is the python implementation for the remote Ariane '
                     'services.'
-                    'Through Ariane CLIP3 you can define your own Ariane external injector which will map your'
-                    'system.')
+                    'Through Ariane CLIP3 you can define your own Ariane external injector which will map your system.'
+                    'Where you can get more informations : '
+                    '   + http://ariane.echinopsii.net'
+                    '   + http://confluence.echinopsii.net/confluence/display/AD/Ariane+Documentation+Home'
+                    '   + IRC on freenode #ariane.echinopsii')
 
 setup(name='ariane_clip3',
-      version='0.1.0-b01',
+      version='0.1.0-b02',
       description='Ariane Python API Library',
       long_description=long_description,
       author='Mathilde Ffrench',
@@ -25,7 +28,7 @@ setup(name='ariane_clip3',
       maintainer='Mathilde Ffrench',
       maintainer_email='mathilde.ffrench@echinopsii.net',
       url='https://github.com/echinopsii/net.echinopsii.ariane.community.cli.python3.git',
-      download_url='https://github.com/echinopsii/net.echinopsii.ariane.community.cli.python3.git/tarball/0.1.0-b01',
+      download_url='https://github.com/echinopsii/net.echinopsii.ariane.community.cli.python3.git/tarball/0.1.0-b02',
       packages=['ariane_clip3'],
       license='AGPLv3',
       install_requires=requirements,
