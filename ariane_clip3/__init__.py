8b7cecc9a800693757f24a10eaea864d02e86ea6
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-07-21 18:51:35 +0200

    [ACC-184] some improvment

--- ariane_clip3/__init__.py
+++ ariane_clip3/__init__.py
@@ -1,14 +1,26 @@
+# Ariane CLI Python 3
+# init
+#
+# Copyright (C) 2015 echinopsii
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 __author__ = 'mffrench'
 __version__ = '0.1.0'
 
 import logging
-try:
-    # not available in python 2.6
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
+from logging import NullHandler
 
 # Add NullHandler to prevent logging warnings
 logging.getLogger(__name__).addHandler(NullHandler())
--- ariane_clip3/exceptions.py
+++ ariane_clip3/exceptions.py
@@ -1,9 +1,27 @@
+# Ariane CLI Python 3
+# Exception
+#
+# Copyright (C) 2015 echinopsii
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 __author__ = 'mffrench'
 
 
 class ArianeError(Exception):
     def __repr__(self):
-        return "Unspecified Ariane Client Error has occured"
+        return "Unspecified Ariane Client Error has occurred"
 
 
 class ArianeConfError(ArianeError):
@@ -14,6 +32,7 @@ class ArianeConfError(ArianeError):
 class ArianeNotImplemented(ArianeError):
     def __repr__(self):
         return self.args[0] + " is not implemented !"
+
 
 class ArianeCallParametersError(ArianeError):
     def __repr__(self):
