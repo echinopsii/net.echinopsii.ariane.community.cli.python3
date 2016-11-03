b01c2990ef7c13a64e73302516320ead35085e51
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-11-03 19:07:54 +0100

    [ACC-489] improve debug logging and add on_failure cb for pykka actor

--- ariane_clip3/zeromq/driver.py
+++ ariane_clip3/zeromq/driver.py
@@ -32,7 +32,6 @@ __author__ = 'mffrench'
 class Publisher(pykka.ThreadingActor):
     """
     ZeroMQ publisher implementation.
-    :param my_args: dict like {connection, service_q, treatment_callback[, service_name]}
     :param connection_args: dict like {user, password, host[, port, vhost, client_properties]}
     """
     def __init__(self, connection_args=None):
@@ -41,6 +40,7 @@ class Publisher(pykka.ThreadingActor):
         :param connection_args: dict like {[host, port]}
         :return: self
         """
+        LOGGER.debug("zeromq.Publisher.__init__")
         super(Publisher, self).__init__()
         self.zmqcontext = zmq.Context.instance()
         self.zmqsocket = self.zmqcontext.socket(zmq.PUB)
@@ -52,6 +52,7 @@ class Publisher(pykka.ThreadingActor):
         :param my_args: dict like {msg: 'msg'}
         :return: nothing
         """
+        LOGGER.debug("zeromq.Publisher.call")
         if my_args is None:
             raise exceptions.ArianeConfError("publisher call arguments")
         if 'topic' not in my_args or my_args['topic'] is None or not my_args['topic']:
@@ -64,16 +65,24 @@ class Publisher(pykka.ThreadingActor):
         """
         start publisher
         """
+        LOGGER.debug("zeromq.Publisher.on_start")
         try:
             self.zmqsocket.bind(self.zmqbind_url)
         except Exception as e:
-            LOGGER.error("error while binding publisher ! " + e.__cause__)
+            LOGGER.error("zeromq.Publisher.on_start - error while binding publisher ! " + e.__cause__)
             raise e
 
     def on_stop(self):
         """
         stop publisher
         """
+        LOGGER.debug("zeromq.Publisher.on_stop")
+        self.zmqsocket.close()
+        self.zmqcontext.destroy()
+
+    def on_failure(self, exception_type, exception_value, traceback_):
+        LOGGER.error("zeromq.Publisher.on_failure - " + exception_type.__str__() + "/" + exception_value.__str__())
+        LOGGER.error("zeromq.Publisher.on_failure - " + traceback_.format_exc())
         self.zmqsocket.close()
         self.zmqcontext.destroy()
 
@@ -91,6 +100,7 @@ class Subscriber(pykka.ThreadingActor):
         :param connection_args: dict like {[host, port]}
         :return: self
         """
+        LOGGER.debug("zeromq.Subscriber.__init__")
         if my_args is None:
             raise exceptions.ArianeConfError("subscriber arguments")
         if 'topic' not in my_args or my_args['topic'] is None or not my_args['topic']:
@@ -98,7 +108,8 @@ class Subscriber(pykka.ThreadingActor):
         if 'treatment_callback' not in my_args or my_args['treatment_callback'] is None:
             raise exceptions.ArianeConfError("treatment_callback")
         if 'subscriber_name' not in my_args or my_args['subscriber_name'] is None or not my_args['subscriber_name']:
-            LOGGER.warn("subscriber_name is not defined ! Use default : " + self.__class__.__name__)
+            LOGGER.warn("zeromq.Subscriber.__init__ - subscriber_name is not defined ! Use default : " +
+                        self.__class__.__name__)
             my_args['subscriber_name'] = self.__class__.__name__
 
         super(Subscriber, self).__init__()
@@ -116,6 +127,7 @@ class Subscriber(pykka.ThreadingActor):
         """
         consume message from channel on the consuming thread.
         """
+        LOGGER.debug("zeromq.Subscriber.run")
         self.running = True
         self.is_started = True
         while self.running:
@@ -125,18 +137,19 @@ class Subscriber(pykka.ThreadingActor):
             except zmq.ZMQError:
                 time.sleep(0.1)
             except Exception as e:
-                LOGGER.warn("Exception raised while consuming on topic " + str(self.zmqtopic))
+                LOGGER.warn("zeromq.Subscriber.run - Exception raised while consuming on topic " + str(self.zmqtopic))
             if msg is not None:
                 try:
                     self.cb(msg)
-                except:
-                    LOGGER.warn("Exception raised while treating msg {" + str(msg) + "}")
+                except Exception as e:
+                    LOGGER.warn("zeromq.Subscriber.run - Exception raised while treating msg {" + str(msg) + "}")
         self.is_started = False
 
     def on_start(self):
         """
         start subscriber
         """
+        LOGGER.debug("zeromq.Subscriber.on_start")
         try:
             self.zmqsocket.connect(self.zmqbind_url)
             self.zmqsocket.setsockopt_string(zmq.SUBSCRIBE, self.zmqtopic)
@@ -150,11 +163,22 @@ class Subscriber(pykka.ThreadingActor):
         """
         stop subscriber
         """
+        LOGGER.debug("zeromq.Subscriber.on_stop")
         self.running = False
         while self.is_started:
             time.sleep(0.1)
         self.zmqsocket.close()
         self.zmqcontext.destroy()
+
+    def on_failure(self, exception_type, exception_value, traceback_):
+        LOGGER.error("zeromq.Subscriber.on_failure - " + exception_type.__str__() + "/" + exception_value.__str__())
+        LOGGER.error("zeromq.Subscriber.on_failure - " + traceback_.format_exc())
+        self.running = False
+        while self.is_started:
+            time.sleep(0.1)
+        self.zmqsocket.close()
+        self.zmqcontext.destroy()
+
 
 class Driver(object):
     """
@@ -166,19 +190,22 @@ class Driver(object):
 
     @staticmethod
     def validate_driver_conf(my_args=None):
+        LOGGER.debug("zeromq.Driver.validate_driver_conf")
         if my_args is None:
-            LOGGER.info("args is not defined. Use default host:port : " +
+            LOGGER.info("zeromq.Driver.validate_driver_conf - args is not defined. Use default host:port : " +
                         str(Driver.default_host) + ":" + str(Driver.default_port))
             my_args = {'host': Driver.default_host, 'port': Driver.default_port}
         else:
             if 'host' not in my_args or my_args['host'] is None or not my_args['host']:
                 my_args['host'] = Driver.default_host
-                LOGGER.info("host is not defined. Use default : " + str(Driver.default_host))
+                LOGGER.debug("zeromq.Driver.validate_driver_conf - host is not defined. Use default : " +
+                             str(Driver.default_host))
             else:
                 my_args['host'] = str(my_args['host'])
             if 'port' not in my_args or my_args['port'] is None or not my_args['port']:
                 my_args['port'] = Driver.default_port
-                LOGGER.info("port is not defined. Use default : " + str(Driver.default_port))
+                LOGGER.debug("zeromq.Driver.validate_driver_conf - port is not defined. Use default : " +
+                             str(Driver.default_port))
             else:
                 my_args['port'] = int(my_args['port'])
         return my_args
@@ -189,6 +216,7 @@ class Driver(object):
         :param my_args: dict like {[host, port]}. Default = None
         :return: self
         """
+        LOGGER.debug("zeromq.Driver.__init__")
         self.type = my_args['type']
         self.configuration_OK = False
         try:
@@ -203,6 +231,7 @@ class Driver(object):
         """
         :return: self
         """
+        LOGGER.debug("zeromq.Driver.start")
         return self
 
     def stop(self):
@@ -210,6 +239,7 @@ class Driver(object):
         Stop ZMQ tools.
         :return: self
         """
+        LOGGER.debug("zeromq.Driver.stop")
         for publisher in self.publishers_registry:
             publisher.stop()
         self.publishers_registry.clear()
@@ -217,7 +247,7 @@ class Driver(object):
             if subscriber.is_started:
                 subscriber.stop()
         self.subscribers_registry.clear()
-        #pykka.ActorRegistry.stop_all()
+        # pykka.ActorRegistry.stop_all()
         return self
 
     def make_service(self, my_args=None):
@@ -225,6 +255,7 @@ class Driver(object):
         not implemented
         :return:
         """
+        LOGGER.debug("zeromq.Driver.make_service")
         raise exceptions.ArianeNotImplemented(self.__class__.__name__ + ".make_service")
 
     def make_requester(self, my_args=None):
@@ -232,6 +263,7 @@ class Driver(object):
         not implemented
         :return:
         """
+        LOGGER.debug("zeromq.Driver.make_requester")
         raise exceptions.ArianeNotImplemented(self.__class__.__name__ + ".make_requester")
 
     def make_publisher(self):
@@ -239,6 +271,7 @@ class Driver(object):
         not implemented
         :return:
         """
+        LOGGER.debug("zeromq.Driver.make_publisher")
         if not self.configuration_OK or self.connection_args is None:
             raise exceptions.ArianeConfError('zeromq connection arguments')
         publisher = Publisher.start(self.connection_args).proxy()
@@ -250,6 +283,7 @@ class Driver(object):
         not implemented
         :return:
         """
+        LOGGER.debug("zeromq.Driver.make_subscriber")
         if my_args is None:
             raise exceptions.ArianeConfError('subscriber arguments')
         if not self.configuration_OK or self.connection_args is None:
