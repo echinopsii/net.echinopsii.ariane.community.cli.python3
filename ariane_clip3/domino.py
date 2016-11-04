b5bfc9f2b5cd88db7d1be20c12e881318c25ab36
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-11-04 17:53:41 +0100

    [ACC-489] add log on domino

--- ariane_clip3/domino.py
+++ ariane_clip3/domino.py
@@ -15,29 +15,37 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import logging
 from ariane_clip3 import driver_factory
 
 __author__ = 'mffrench'
 
+LOGGER = logging.getLogger(__name__)
+
 class DominoActivator(object):
     def __init__(self, driver_args):
+        LOGGER.debug("DominoActivator.__init__")
         self.driver = driver_factory.DriverFactory.make(driver_args)
         self.driver.start()
         self.publisher = self.driver.make_publisher()
 
     def activate(self, topic):
+        LOGGER.debug("DominoActivator.activate - " + topic)
         self.publisher.call({'topic': topic, 'msg': "GO"}).get()
 
     def stop(self):
+        LOGGER.debug("DominoActivator.stop")
         self.driver.stop()
         self.publisher = None
 
 class DominoReceptor(object):
     def __init__(self, driver_args, receptor_args):
+        LOGGER.debug("DominoReceptor.__init__")
         self.driver = driver_factory.DriverFactory.make(driver_args)
         self.driver.start()
         self.subscriber = self.driver.make_subscriber(my_args=receptor_args)
 
     def stop(self):
+        LOGGER.debug("DominoReceptor.stop")
         self.driver.stop()
         self.subscriber = None
